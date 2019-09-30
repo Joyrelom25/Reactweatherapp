03b0b264d44da376e6f98a68b03f2fe1f0ad93cd
Joyrelom25 <54837919+Joyrelom25@users.noreply.github.com>
2019-09-30 20:57:46 +0200

    123

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import Titles from './components/Titles'
+import Form from './components/Form'
+import Weather from './components/Weather'
+
+const API_KEY = "39a3cca998a3e422c540f1b85dc5aa22"
+
 
 class App extends React.Component {
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch();
+    const data = await api_call.json();
+
+    console.log(data);
+
+  }
+
   render() {
     return(
       <div>
-        <p> Hello! </p>
-        <h1> yo! </h1>
-
+        <Titles / >
+        <Form getWeather={this.getWeather} / >
+        <Weather / >
       </div>
     );
   }
