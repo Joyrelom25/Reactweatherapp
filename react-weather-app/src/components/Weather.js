a8e871f1b83fcffbdf29df91ca9ffd3903e4f8a3
Joyrelom25 <54837919+Joyrelom25@users.noreply.github.com>
2019-10-02 16:27:56 +0200

    7890

--- react-weather-app/src/components/Weather.js
+++ react-weather-app/src/components/Weather.js
@@ -3,15 +3,15 @@ import React from 'react'
 class Weather extends React.Component {
     render() {
         return(
-         <div> 
-         {this.props.city && this.props.country &&  <p> Location : {this.props.city} {this.props.country} </p> }
-          {this.props.temperature &&  <p>Temperature : {this.props.temperature}</p> }
-          {this.props.humidity && <p> Humidity : {this.props.humidity} </p> }
-          {this.props.description &&  <p>  Description : {this.props.description} </p> }
-          {this.props.tempmax && <p> Temp max : {this.props.tempmax}</p>}
-          {this.props.tempmin && <p> Temp min : {this.props.tempmin}</p>}
-          {this.props.datetime && <p> Date / time : {this.props.datetime}</p>}
-          {this.props.error && <p>{this.props.error}</p>}
+         <div className = "weather__info"> 
+            {this.props.city && this.props.country &&  <p className= "weather__key"> Location : {this.props.city} {this.props.country} </p> }
+            {this.props.temperature &&  <p className= "weather__key"> Temperature : {this.props.temperature}</p> }
+            {this.props.humidity && <p className= "weather__key"> Humidity : {this.props.humidity} </p> }
+            {this.props.description &&  <p className= "weather__key">  Description : {this.props.description} </p> }
+            {this.props.tempmax && <p className= "weather__key"> Temp max : {this.props.tempmax}</p>}
+            {this.props.tempmin && <p className= "weather__key" > Temp min : {this.props.tempmin}</p>}
+            {this.props.datetime && <p className= "weather__key">  Date / time : {this.props.datetime}</p>}
+            {this.props.error && <p className= "weather__error">{this.props.error}</p>}
         </div>       
         );
     }
