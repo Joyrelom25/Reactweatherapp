3f8caf69e741e77960d224d68ee6f6858e12091a
Joyrelom25 <54837919+Joyrelom25@users.noreply.github.com>
2019-10-03 12:09:00 +0200

    newcomp

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Titles from './components/Titles'
 import Form from './components/Form'
 import Weather from './components/Weather'
-import WeatherIcons from './components/weatherIcons'
+import 'weather-icons/css/weather-icons.css'
 
 const API_KEY = "39a3cca998a3e422c540f1b85dc5aa22"
 
@@ -11,8 +11,8 @@ class App extends React.Component {
 
   state = {
 
- /*   rangeId: undefined,
-    icon : undefined, */ 
+    rangeId: undefined,
+    icon : undefined, 
     temperature: undefined,
     city: undefined,
     country: undefined,
@@ -24,32 +24,35 @@ class App extends React.Component {
 
   }
 
- /*  getWeatherIcons(icon, rangeId) {
-    switch(true) {
-      case rangeId >= 200 && rangeId <= 232:
-        this.setState({icon: this.WeatherIcons.Thunderstorm})
-        break;
-        case rangeId >= 300 && rangeId <= 321:
-        this.setState({icon: this.WeatherIcons.Drizzle})
-        break;
-        case rangeId >= 500 && rangeId <= 531:
-        this.setState({icon: this.WeatherIcons.Rain})
-        break;
-        case rangeId >= 600 && rangeId <= 622:
-        this.setState({icon: this.WeatherIcons.Snow})
-        break;
-        case rangeId >= 701 && rangeId <= 781:
-        this.setState({icon: this.WeatherIcons.Atmosphere})
-        break;
-        case rangeId >= 801 && rangeId <= 804:
-        this.setState({icon: this.WeatherIcons.Clouds})
-        break;
-        case rangeId == 800:
-        this.setState({icon: this.WeatherIcons.Clear})
-        break;
-        default: this.setState({icon: this.WeatherIcons.Clear})
-    }
-  } */
+      getWeatherIcons(rangeId) {
+
+        if (rangeId >= 200 && rangeId <= 232) {
+          return "wi wi-thunderstorm";
+        }
+        if (rangeId >= 300 && rangeId <= 321) {
+          return "wi wi-sleet";
+        }
+        if (rangeId >= 500 && rangeId <= 531) {
+          return "wi wi-storm-showers";
+        }
+        if (rangeId >= 600 && rangeId <= 622) {
+          return "wi wi-snow";
+        }
+        if (rangeId >= 701 && rangeId <= 781) {
+          return "wi wi-fog";
+        }
+        if (rangeId >= 801 && rangeId <= 804) {
+          return "wi wi-day-fog";
+        }
+        if (rangeId === 800) {
+          return "wi wi-day-sunny";
+        }
+        if (rangeId >= 200 && rangeId <= 232) {
+          return "wi wi-thunderstorm";
+        }
+       
+        return "wi wi-day-sunny"
+   } 
 
   getWeather = async (e) => {
     e.preventDefault();
@@ -57,64 +60,66 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
+    this.getWeatherIcons(Number(data.weather[0].id));
+   
+    if (city && country) {
 
-    if (city && country ) {
-    console.log(data);
-      
-    this.setState({
+      console.log(data)
 
- /*     rangeId: data.weather[0].id,
-      icon : this.getWeatherIcons , */ 
-      temperature: data.main.temp, 
-      city: data.name,
-      country: data.sys.country,
-      description: data.weather[0].description,
-      tempmax: data.main.temp_max,
-      tempmin: data.main.temp_min,
-      datetime: data.dt*1000,
-      error: ""
+      this.setState({
 
-    });
+        rangeId: data.weather[0].id,
+        icon : this.getWeatherIcons(data.weather[0].id) , 
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        description: data.weather[0].description,
+        tempmax: data.main.temp_max,
+        tempmin: data.main.temp_min,
+        datetime: new Date(data.dt *1000).toString(),
+        error: ""
+
+      });
 
     } else {
       this.setState({
- /*     rangeId: undefined,  
-        icon : undefined, */
-        temperature: undefined, 
+        rangeId: undefined,  
+        icon : undefined,
+        temperature: undefined,
         city: undefined,
-       country: undefined,
+        country: undefined,
         description: undefined,
-       tempmax: undefined,
-       tempmin: undefined,
+        tempmax: undefined,
+        tempmin: undefined,
         datetime: undefined,
-       error: "Please enter the values"
+        error: "Please enter the values"
       })
     }
   }
 
   render() {
-    return(
+    return (
       <div>
-        <div className ="wrapper">
-          <div className = "main">
-            <div className= "container">
-              <div className = "row"> </div>
-               <div className = "col-xs-5 title-container">
-                  <Titles / >
-               </div>
-               <div className = "col-xs-7 form-container">
-               <Form getWeather={this.getWeather} / >
-                  <Weather
-                     /* icon = {this.state.icon} */ 
-                      temperature={this.state.temperature}
-                      city= {this.state.city}
-                      country= {this.state.country}
-                      description= {this.state.description}
-                      tempmax = {this.state.tempmax}
-                      tempmin = {this.state.tempmin}
-                      datetime = {this.state.datetime}
-                      error= {this.state.error} / >
-               </div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row"> </div>
+              <div className="col-xs-5 title-container">
+                <Titles />
+              </div>
+              <div className="col-xs-7 form-container">
+                <Form getWeather={this.getWeather} />
+                <Weather
+                  icon = {this.state.icon}  
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  description={this.state.description}
+                  tempmax={this.state.tempmax}
+                  tempmin={this.state.tempmin}
+                  datetime={this.state.datetime}
+                  error={this.state.error} />
+              </div>
             </div>
           </div>
         </div>
--- react-weather-app/src/components/weatherIcons.js
+++ react-weather-app/src/components/weatherIcons.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import 'weather-icons/css/weather-icons.css'
+// import React from 'react'
+// import 'weather-icons/css/weather-icons.css'
 
-class WeatherIcons extends React.Component {
-    state = {
-        Thunderstorm : "wi-thunderstorm",
-        Drizzle: "wi-sleet",
-        Rain: "wi-storm-showers",
-        Snow: "wi-snow",
-        Atmosphere: "wi-fog",
-        Clear: "wi-day-sunny",
-        Clouds: "wi-day-fog"
-    }
-}
+// class WeatherIcons extends React.Component {
+//     state = {
+//         Thunderstorm : "wi-thunderstorm",
+//         Drizzle: "wi-sleet",
+//         Rain: "wi-storm-showers",
+//         Snow: "wi-snow",
+//         Atmosphere: "wi-fog",
+//         Clear: "wi-day-sunny",
+//         Clouds: "wi-day-fog"
+//     }
+// }
 
-export default WeatherIcons;
+// export default WeatherIcons;
